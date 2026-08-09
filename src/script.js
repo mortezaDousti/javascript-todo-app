aac620b9054dc3339b4167c4557567ce95b16ee7
morteza dousti <morteza.dousti.dev@gmail.com>
2026-08-09 07:56:33 +0330

    Save completed task state in localStorage

--- src/script.js
+++ src/script.js
@@ -1,24 +1,22 @@
 const input = document.querySelector('#taskInput');
 const button = document.querySelector('#addBtn');
 const list = document.querySelector('.taskList');
-
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 button.addEventListener('click', addTask);
 
 function addTask() {
   if (input.value.trim() === '') {
     return;
   }
-  const taskText = input.value;
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.textContent = taskText;
-  span.addEventListener('click', () => span.classList.toggle('completed'));
-  const btn = document.createElement('button');
-  btn.textContent = 'Del';
-  btn.addEventListener('click', () => li.remove());
-  li.appendChild(span);
-  li.appendChild(btn);
-  list.appendChild(li);
+  const taskText = input.value.trim();
+  const newTask = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  tasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  renderTasks();
   input.value = '';
   input.focus();
 }
@@ -28,3 +26,43 @@ input.addEventListener('keydown', (event) => {
     addTask();
   }
 });
+
+function renderTasks() {
+  list.innerHTML = '';
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+
+    //===== Span for writing the tasks =====
+
+    const span = document.createElement('span');
+    span.textContent = task.title;
+    if (task.completed) {
+      span.classList.add('completed');
+    }
+    span.addEventListener('click', () => {
+      task.completed = !task.completed;
+      if (task.completed) {
+        span.classList.add('completed');
+      }
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+
+      renderTasks();
+    });
+    //===== Button del =====
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Del';
+    btn.addEventListener('click', () => {
+      tasks = tasks.filter((item) => item.id !== task.id);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      renderTasks();
+    });
+
+    //===== Create element =====
+
+    li.appendChild(span);
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+}
+renderTasks();
